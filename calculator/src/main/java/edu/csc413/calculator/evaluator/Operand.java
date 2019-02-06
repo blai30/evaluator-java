4aee58f67ad69ac8457b617688a10cac9664bffa
Brian Lai <brianlai30@outlook.com>
2019-02-05 16:33:45 -0800

    Reformatted Evaluator and Operator to 4 spaces

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
@@ -4,29 +4,29 @@ package edu.csc413.calculator.evaluator;
  * in a valid mathematical expression.
  */
 public class Operand {
-  /**
-  * construct operand from string token.
-  */  
-  public Operand( String token ) {
-    
-  }
-  /**
-   * construct operand from integer
-   */
-  public Operand( int value ) {
-    
-  }
-  /**
-   * return value of opernad
-   */
-  public int getValue() {
-      return 0;
-  }
-  /**
-   * Check to see if given token is a valid
-   * operand.
-   */
-  public static boolean check( String token ) {
-    return false;
-  }
+    /**
+     * construct operand from string token.
+     */
+    public Operand( String token ) {
+
+    }
+    /**
+     * construct operand from integer
+     */
+    public Operand( int value ) {
+
+    }
+    /**
+     * return value of opernad
+     */
+    public int getValue() {
+        return 0;
+    }
+    /**
+     * Check to see if given token is a valid
+     * operand.
+     */
+    public static boolean check( String token ) {
+        return false;
+    }
 }
