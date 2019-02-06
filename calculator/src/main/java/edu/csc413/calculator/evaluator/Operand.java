3868f7001b8ae00589447d1e0936c924885cc566
Brian Lai <brianlai30@outlook.com>
2019-02-05 19:50:51 -0800

    Filled in code for Operand

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
@@ -4,25 +4,32 @@ package edu.csc413.calculator.evaluator;
  * in a valid mathematical expression.
  */
 public class Operand {
+    private int operand;
+
     /**
      * construct operand from string token.
      */
     public Operand(String token) {
-
+        try {
+            operand = Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            operand = 0;
+            System.out.println("Exception: string cannot be parsed into int. operand set to 0");
+        }
     }
 
     /**
      * construct operand from integer
      */
     public Operand(int value) {
-
+        operand = value;
     }
 
     /**
-     * return value of opernad
+     * return value of operand
      */
     public int getValue() {
-        return 0;
+        return operand;
     }
 
     /**
@@ -30,6 +37,11 @@ public class Operand {
      * operand.
      */
     public static boolean check(String token) {
-        return false;
+        try {
+            Integer.parseInt(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
