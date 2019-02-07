da9ac4dfb914d5335f3b71836333f20791fe2737
Brian Lai <brianlai30@outlook.com>
2019-02-06 18:04:45 -0800

    Added Overrides to operator files

--- calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
@@ -1,7 +1,6 @@
 package edu.csc413.calculator.operators;
 
 import edu.csc413.calculator.evaluator.Operand;
-import edu.csc413.calculator.operators.Operator;
 
 public class AddOperator extends Operator {
     @Override
--- calculator/src/main/java/edu/csc413/calculator/operators/SubtractOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/SubtractOperator.java
@@ -1,7 +1,15 @@
 package edu.csc413.calculator.operators;
 
-import edu.csc413.calculator.operators.Operator;
+import edu.csc413.calculator.evaluator.Operand;
 
-public class SubtractOperator {
+public class SubtractOperator extends Operator {
+    @Override
+    public int priority() {
+        return 1;
+    }
 
+    @Override
+    public Operand execute(Operand op1, Operand op2) {
+        return null;
+    }
 }
